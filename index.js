d1eef3ccd1f0a98d1215b75a43874bdfabddf0aa
Franco <franco.neville.1@gmail.com>
2022-09-15 20:32:23 -0300

    Updated App according to exercise 3.21

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ const errorHandler = (error, request, response, next) => {
 
    if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+   } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
    }
+   
    next(error)
 }
 
@@ -45,7 +48,7 @@ app.use(morgan(":method :url :status - :response-time ms - :body"))
 
 
 // routes handlers
-app.get("/info", (request, response) => {
+app.get("/info", (request, response, next) => {
    Person.find({})
       .then(result => {
          response.send(
@@ -60,7 +63,7 @@ app.get("/info", (request, response) => {
       .catch(error => next(error))
 })
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
    Person.find({})
       .then(list => {
          response.json(list)
@@ -68,7 +71,7 @@ app.get("/api/persons", (request, response) => {
       .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
    Person.findById(request.params.id)
       .then(person => {
          if (person) {
@@ -80,7 +83,7 @@ app.get("/api/persons/:id", (request, response) => {
       .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
    const body = request.body;
 
    if (!body.name || !body.number) {
@@ -99,7 +102,7 @@ app.post("/api/persons", (request, response) => {
       .catch(error => next(error))
 })
 
-app.put("/api/persons/:id", (request, response) => {
+app.put("/api/persons/:id", (request, response, next) => {
    const body = request.body
 
    if (!body.name || !body.number) {
@@ -111,17 +114,19 @@ app.put("/api/persons/:id", (request, response) => {
       number: body.number
    } 
 
-   Person.findByIdAndUpdate(request.params.id, updatedPerson, { new: true })
+   Person.findByIdAndUpdate(request.params.id, updatedPerson, { new: true, runValidators: true, context: 'query' })
       .then(result => {
          response.json(result)
       })
+      .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
    Person.findByIdAndRemove(request.params.id)
       .then(result => {
          response.status(204).end()
       })
+      .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
